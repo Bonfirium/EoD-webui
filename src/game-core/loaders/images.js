0ed0835fd5aac3be69efb62a8966b7bc6dd4b6b0
djoma <d.mosko@pixelplex.by>
2018-10-06 21:09:02 +0300

    refactor image loader

--- src/game-core/loaders/images.js
+++ src/game-core/loaders/images.js
@@ -1,20 +1,19 @@
 import * as PIXI from 'pixi.js';
 import BaseLoader from './_base';
-import Chest from '../../assets/images/game/chest_128.png';
-import Door from '../../assets/images/game/door_38.png';
-import DoorR from '../../assets/images/game/door_38_R.png';
-import Hero_1 from '../../assets/images/game/hero_01_true.png';
-import Hero_2 from '../../assets/images/game/hero_02_true.png';
-import Hero_3 from '../../assets/images/game/hero_03_true.png';
-import Hero_4 from '../../assets/images/game/hero_04_true.png';
-import Org_1 from '../../assets/images/game/ogr_01_true.png';
-import Org_2 from '../../assets/images/game/ogr_02_true.png';
-import Org_3 from '../../assets/images/game/ogr_03_true.png';
-import Org_4 from '../../assets/images/game/ogr_04_true.png';
-import Org_5 from '../../assets/images/game/ogr_05_true.png';
-import Portal from '../../assets/images/game/portal_true_90.png';
-import Room from '../../assets/images/game/room_128.png';
-import Background from '../../assets/images/game/back_2000.png';
+import chest from '../../assets/images/game/chest_128.png';
+import door from '../../assets/images/game/door_38.png';
+import doorInverted from '../../assets/images/game/door_38_R.png';
+import hero1 from '../../assets/images/game/hero_01_true.png';
+import hero2 from '../../assets/images/game/hero_02_true.png';
+import hero3 from '../../assets/images/game/hero_03_true.png';
+import hero4 from '../../assets/images/game/hero_04_true.png';
+import monster1 from '../../assets/images/game/ogr_01_true.png';
+import monster2 from '../../assets/images/game/ogr_02_true.png';
+import monster3 from '../../assets/images/game/ogr_03_true.png';
+import monster4 from '../../assets/images/game/ogr_04_true.png';
+import portal from '../../assets/images/game/portal_true_90.png';
+import room from '../../assets/images/game/room_128.png';
+import background from '../../assets/images/game/back_2000.png';
 
 /**
  *  ## USAGE
@@ -23,29 +22,19 @@ import Background from '../../assets/images/game/back_2000.png';
  * `new Sprite(Images[SomeImage]);`
  * @type {Object.<String, PIXI.Texture>}
  */
-export const Images = {};
+export let Images = {};
 
-export default class ImagesLoader extends BaseLoader {
+export default class ImagesLoader {
 
 	constructor() {
-		super();
-		this.imagesToLoad = [
-			Chest, Door, DoorR,
-			Hero_1, Hero_2, Hero_3, Hero_4,
-			Org_1, Org_2, Org_3, Org_4, Org_5,
-			Portal, Room, Background
-		];
-		this.imagesToLoad.forEach((image) => {
-			// resource id is a same as path to this image
-			PIXI.loader.add(image, image);
-		});
-	}
-
-	/** @inheritDoc BaseLoader#onLoad */
-	onLoad(_, resources) {
-		this.imagesToLoad.forEach((image) => {
-			Images[image] = resources[image].texture;
-		});
+		const loader = new PIXI.loaders.Loader();
+		Object.keys({
+			chest, door, doorInverted,
+			hero1, hero2, hero3, hero4,
+			monster1, monster2, monster3, monster4,
+			portal, room, background,
+		}).forEach((name) => { loader.add(name, imagesToLoad[name]); });
+		loader.load((_, resources) => { Images = resources; });
 	}
 
 }
