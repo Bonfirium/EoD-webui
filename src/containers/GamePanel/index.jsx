d92be5e1de7e79315a1a7099021a60839f6468b9
Kriket <kriket267@gmail.com>
2018-10-18 02:18:37 +0300

    game interface

--- src/containers/GamePanel/index.jsx
+++ src/containers/GamePanel/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react';
 
 class GamePanel extends React.Component {
 
 	render() {
 
 		return (
-			<div className="main_body text-gray">
+			<div className="main_body text-gray interface">
+				<div className="header-center-wrap">
+					<Menu stackable className="header-wrap">
+						<Menu.Item className="semi-wide" as="div">
+							123
+						</Menu.Item>
+						<Menu.Item className="semi-wide" as="div">
+							Wins: 10
+						</Menu.Item>
+						<Menu.Item className="semi-wide">
+							Defeats: 3
+						</Menu.Item>
+						<Menu.Item className="semi-wide" position="right">
+							0
+						</Menu.Item>
+					</Menu>
+				</div>
 				<div id="game-container" />
 			</div>
 		);
